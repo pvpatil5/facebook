712a835a60bad478cf59708b54544471b86b6ebc
Pinjariirfan7878 <Pinjariirfan7878gmail.com>
2022-08-02 11:59:45 +0530

    msg

---
+++ src/test/java/Pac_facebook/FB_join.java
@@ -0,0 +1,18 @@
+package Pac_facebook;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class FB_join {
+	@Test
+	public void runFacebook()
+	{
+		WebDriverManager.firefoxdriver().setup();
+		WebDriver driver=new FirefoxDriver();
+		driver.get("http://www.facebook.com/");
+	}
+
+}
